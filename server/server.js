cd7ce88d56cf801abf8207e169324d43571ee5e2
YVictorin <vicyonjou@gmail.com>
2025-02-01 19:18:17 -0500

    wip: update logic for vercel deployment

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
 
-import indexRouter from "./src/routes/index.js";
+// import indexRouter from "./src/routes/index.js";
 import userRouter from "./src/routes/users.js"; 
 import productRouter from "./src/routes/products.js";
 import supportBotRouter from "./src/routes/supportBotRoute.js"
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 // use routers
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/api/users', userRouter); 
 app.use('/api/products', productRouter);
 app.use('/api/supportBot', supportBotRouter)
@@ -44,11 +44,6 @@ app.use('/api/account', accountRoute)
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: "Internal server error", error: err.message });
-});
-
-// Redirect all requests to index.html for frontend routing (React, Vue, etc.)
-app.get("*", (req, res) => {
-    res.sendFile( path.resolve(__dirname, '../../../client/dist/index.html'));
 });
 
 app.listen(SERVER_PORT, () => {
